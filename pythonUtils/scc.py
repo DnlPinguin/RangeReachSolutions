a420a7c264c630993394bc9fd92fc26235374227
DnlPinguin <daniel.valentink@gmail.com>
2022-05-06 11:01:56 +0200

    checks

--- pythonUtils/scc.py
+++ pythonUtils/scc.py
@@ -19,8 +19,20 @@ def check_dag(file_name):
 
     print("Graph read")
 
-    print("Is Graph strongly connected ", networkx.is_strongly_connected(G_G))
-    print("Is Graph weakly connected ", networkx.is_weakly_connected(G_G))
+    # print("Is Graph strongly connected ", networkx.is_strongly_connected(G_G))
+    # print("Is Graph weakly connected ", networkx.is_weakly_connected(G_G))
+    wcc = networkx.weakly_connected_components(G_G)
+    for super_connected_component in wcc:
+        super_connected_component = list(super_connected_component)
+        print(len(super_connected_component))
+        # max_node = max_node + 1
+        # scc_dict[max_node] = super_connected_component
+        # scc_file.write(str(max_node))
+        # for i in super_connected_component:
+        #     scc_file.write("\t" + str(i))
+        #         node_belongs_to_scc[i] = max_node
+        #     scc_file.write("\n")
+        # del(super_connected_component)
     print("Number of weakly connected components ", networkx.number_weakly_connected_components(G_G))
     print("Number of strongly connected components: ", networkx.number_strongly_connected_components(G_G))
     print("Number of nodes: ", len(G_G))
