e8621b53395e2a8b45ecc46a10633aec3e1d6b84
DnlPinguin <daniel.valentink@gmail.com>
2022-05-19 18:16:12 +0200

    wcc checker

---
+++ pythonUtils/createBiggestWCC.py
@@ -0,0 +1,9 @@
+import postorder
+import scc
+
+print("Specify file name:")
+
+file = input()
+
+
+scc.create_greateast_weakyl_connected_component(file)
--- pythonUtils/run.py
+++ pythonUtils/run.py
@@ -5,5 +5,6 @@ print("Specify file name:")
 
 file = input()
 
+
 scc.construct_super_nodes_on_graph(file)
 postorder.construct_postorder_on_nodes(file)
